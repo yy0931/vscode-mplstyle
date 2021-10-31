6b12d0dcefbe89db5878348b30d5466f5f762b62
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-11-01 06:43:35 +0900

    Add a CI

---
+++ update_matplotlib_source.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+REPO=https://raw.githubusercontent.com/matplotlib/matplotlib/main
+curl "$REPO/lib/matplotlib/rcsetup.py" --create-dirs -o matplotlib/lib/matplotlib/rcsetup.py
+curl "$REPO/lib/matplotlib/mpl-data/matplotlibrc" --create-dirs -o matplotlib/lib/matplotlib/mpl-data/matplotlibrc
+curl "$REPO/LICENSE/LICENSE" --create-dirs -o matplotlib/LICENSE/LICENSE
