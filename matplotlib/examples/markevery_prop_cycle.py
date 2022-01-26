8e5e0913faeac83a6564b604fd442b0bd928b2c7
yy0931 <54441600+yy0931@users.noreply.github.com>
2022-01-26 19:21:09 +0900

    Fix examples/markevery_prop_cycle.py
    Changed not to overwrite the color cycle

--- matplotlib/examples/markevery_prop_cycle.py
+++ matplotlib/examples/markevery_prop_cycle.py
@@ -30,21 +30,6 @@ cases = [None,
          (0.0, 0.1),
          (0.45, 0.1)]
 
-colors = ['#1f77b4',
-          '#ff7f0e',
-          '#2ca02c',
-          '#d62728',
-          '#9467bd',
-          '#8c564b',
-          '#e377c2',
-          '#7f7f7f',
-          '#bcbd22',
-          '#17becf',
-          '#1a55FF']
-
-# Configure rcParams axes.prop_cycle to simultaneously cycle cases and colors.
-mpl.rcParams['axes.prop_cycle'] = cycler(markevery=cases, color=colors)
-
 # Create data points and offsets
 x = np.linspace(0, 2 * np.pi)
 offsets = np.linspace(0, 2 * np.pi, 11, endpoint=False)
