798a2ea973beff17532c45b5a0e90c21495512a2
yy0931 <54441600+yy0931@users.noreply.github.com>
2022-03-12 18:23:23 +0900

    Parallelize generate_documentation_images.py

--- scripts/generate_documentation_images.py
+++ scripts/generate_documentation_images.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import io
+import multiprocessing
 import shutil
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Sequence
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -15,61 +16,58 @@ from matplotlib.figure import Figure
 out_dir = Path(__file__).parent.parent / "example"
 
 
-def savefig(out_file: Path):
-    # Render the figure
-    f = io.BytesIO()
-    plt.savefig(f, format="png")
-    im = PIL.Image.open(f)
-    out_dir.mkdir(parents=True, exist_ok=True)
-
-    # Crop the image
-    im = im.crop(im.getbbox())
-    im.save(out_file)
-    plt.close()
+def plot_capstyle_simple(args: Sequence[tuple[str, str]]):
+    def f(ax: Axes, title: str):
+        ax.plot([1, 2.5, 4], [0.4, 0, 0.3])
+        ax.set_xlabel('xlabel')
+        ax.set_ylabel('ylabel')
+        ax.set_title(title)
+    render_figure(f, *args[0], args[1:])
 
 
-def plot_capstyle_simple(ax: Axes, title: str):
-    ax.plot([1, 2.5, 4], [0.4, 0, 0.3])
-    ax.set_xlabel('xlabel')
-    ax.set_ylabel('ylabel')
-    ax.set_title(title)
+def plot_axes_simple(args: Sequence[tuple[str, str]]):
+    def f(ax: Axes, title: str):
+        x1 = np.linspace(0.0, 5.0)
+        y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+        ax.plot(x1, y1)
+        ax.set_xlabel('xlabel')
+        ax.set_ylabel('ylabel')
+        ax.set_title(title)
+    render_figure(f, *args[0], args[1:])
 
 
-def plot_axes_simple(ax: Axes, title: str):
-    x1 = np.linspace(0.0, 5.0)
-    y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
-    ax.plot(x1, y1)
-    ax.set_xlabel('xlabel')
-    ax.set_ylabel('ylabel')
-    ax.set_title(title)
+def plot_axes_legend(args: Sequence[tuple[str, str]]):
+    def f(ax: Axes, title: str):
+        x = np.linspace(0, 1, num=8)
+        for (n, marker) in ((1, "o"), (2, "^")):
+            ax.plot(x, x**n, label="n={0}".format(n), marker=marker)
+        ax.legend().set_title("legend title")
+        ax.set_xlabel('xlabel')
+        ax.set_ylabel('ylabel')
+        ax.set_title(title)
+    render_figure(f, *args[0], args[1:])
 
 
-def plot_axes_legend(ax: Axes, title: str):
-    x = np.linspace(0, 1, num=8)
-    for (n, marker) in ((1, "o"), (2, "^")):
-        ax.plot(x, x**n, label="n={0}".format(n), marker=marker)
-    ax.legend().set_title("legend title")
-    ax.set_xlabel('xlabel')
-    ax.set_ylabel('ylabel')
-    ax.set_title(title)
+def plot_axes_legend_col2(args: Sequence[tuple[str, str]]):
+    def f(ax: Axes, title: str):
+        x = np.linspace(0, 1, num=8)
+        for (n, marker) in ((1, "o"), (2, "^")):
+            ax.plot(x, x**n, label="n={0}".format(n), marker=marker)
+        ax.legend(ncol=2).set_title("legend title")
+        ax.set_xlabel('xlabel')
+        ax.set_ylabel('ylabel')
+        ax.set_title(title)
+    render_figure(f, *args[0], args[1:])
 
 
-def plot_axes_legend_col2(ax: Axes, title: str):
-    x = np.linspace(0, 1, num=8)
-    for (n, marker) in ((1, "o"), (2, "^")):
-        ax.plot(x, x**n, label="n={0}".format(n), marker=marker)
-    ax.legend(ncol=2).set_title("legend title")
-    ax.set_xlabel('xlabel')
-    ax.set_ylabel('ylabel')
-    ax.set_title(title)
+def initialize_worker():
+    plt.style.use(Path(__file__).parent / 'documentation-images.mplstyle')
 
 
-def render_figure(plot_axes: Callable[[Axes, str], None], key: str, value: str, defaults: list[tuple[str, str]]):
+def render_figure(plot_axes: Callable[[Axes, str], None], key: str, value: str, defaults: Sequence[tuple[str, str]]):
     """https://matplotlib.org/stable/gallery/subplots_axes_and_figures/subplot.html"""
-
     def render_fig():
-        fig: Figure
-        fig = plt.figure()
+        fig: Figure = plt.figure()
         with matplotlib.rc_context({key: value}):
             plot_axes(fig.add_subplot(121), matplotlib.rcParams[key])  # type: ignore
         plot_axes(fig.add_subplot(122), matplotlib.rcParams[key])  # type: ignore
@@ -82,15 +80,26 @@ def render_figure(plot_axes: Callable[[Axes, str], None], key: str, value: str, 
     with matplotlib.rc_context({k: v for k, v in defaults}):
         with matplotlib.rc_context({"figure.facecolor": "none"}):
             bbox = render_fig().getbbox()
-        out_dir.mkdir(parents=True, exist_ok=True)
         render_fig().crop(bbox).save(out_dir / f"{key}.png")
 
 
-shutil.rmtree(out_dir, ignore_errors=True)
+def parse_config(config: str):
+    for line in config.splitlines():
+        if line.strip() == "":
+            continue
 
-plt.style.use(Path(__file__).parent / 'documentation-images.mplstyle')
+        values: list[tuple[str, str]] = []
+        for pair in line.split(";"):
+            k, v = pair.split(":", 1)
+            values.append((k.strip(), v.strip()))
+        yield values
 
-todo = """
+
+if __name__ == "__main__":
+    shutil.rmtree(out_dir, ignore_errors=True)
+    out_dir.mkdir(parents=True)
+
+    todo = """
 axes.autolimit_mode: round_numbers
 axes.formatter.limits: -1, 1
 axes.formatter.min_exponent: 5
@@ -114,20 +123,8 @@ ytick.minor.pad: 6.8;      xtick.minor.visible: True; ytick.minor.visible: True
 ytick.minor.right: False;  xtick.minor.visible: True; ytick.minor.visible: True
 """
 
-
-def parse_config(config: str):
-    for line in config.splitlines():
-        if line.strip() == "":
-            continue
-
-        values: list[tuple[str, str]] = []
-        for pair in line.split(";"):
-            k, v = pair.split(":", 1)
-            values.append((k.strip(), v.strip()))
-        yield values
-
-
-for values in parse_config("""
+    with multiprocessing.Pool(initializer=initialize_worker) as p:
+        p.map(plot_axes_simple, parse_config("""
 axes.edgecolor: green; axes.linewidth: 1.5
 axes.facecolor: lightgreen
 axes.grid: True
@@ -200,22 +197,20 @@ ytick.minor.size: 4;       xtick.minor.visible: True; ytick.minor.visible: True
 xtick.minor.width: 5;      xtick.minor.visible: True; ytick.minor.visible: True
 ytick.minor.width: 5;      xtick.minor.visible: True; ytick.minor.visible: True
 ytick.minor.left: False;   xtick.minor.visible: True; ytick.minor.visible: True
-"""):
-    render_figure(plot_axes_simple, *values[0], values[1:])
+"""))
 
-for values in parse_config("""
+        p.map(plot_capstyle_simple, parse_config("""
 lines.solid_capstyle: round; lines.linewidth: 15; axes.xmargin: 0.5; axes.ymargin: 0.3
 lines.solid_joinstyle: miter; lines.linewidth: 15; axes.xmargin: 0.5; axes.ymargin: 0.3
 lines.dash_capstyle: round; lines.linestyle: dashed; lines.linewidth: 5; lines.linewidth: 10
 lines.dash_joinstyle: miter; lines.linestyle: dashed; lines.linewidth: 5; lines.linewidth: 10; axes.xmargin: 0.2; axes.ymargin: 0.3
-"""):
-    render_figure(plot_capstyle_simple, *values[0], values[1:])
+"""))
 
-todo = """
+        todo = """
 legend.scatterpoints: 2
 """
 
-for values in parse_config("""
+        p.map(plot_axes_legend, parse_config("""
 legend.loc: center
 legend.frameon: False
 legend.framealpha: 0.2; axes.facecolor: lightgreen; legend.edgecolor: black
@@ -233,12 +228,9 @@ legend.borderaxespad: 2.5
 legend.numpoints: 2
 legend.markerscale: 2.0
 legend.fancybox: False; legend.edgecolor: green
-"""):
-    render_figure(plot_axes_legend, *values[0], values[1:])
-
-for values in parse_config("""
+"""))
+        p.map(plot_axes_legend_col2, parse_config("""
 legend.columnspacing: 4
-"""):
-    render_figure(plot_axes_legend_col2, *values[0], values[1:])
+"""))
 
-(out_dir / "index.txt").write_text("\n".join(f.name for f in out_dir.iterdir()))
+    (out_dir / "index.txt").write_text("\n".join(f.name for f in out_dir.iterdir()))
